f75ae8479e7e0dd28f41b03fd02119f1774f0d4b
complexitycollapse <complexitycollapse@protonmail.com>
2025-01-03 14:43:34 +0000

    Lexer now incremental

--- language/lexer-component.jsx
+++ language/lexer-component.jsx
@@ -40,48 +40,94 @@ export class Lexer {
   constructor(code) {
     this.lines = code.split("\n");
     this.index = 0;
+    this.currentLineLexer = undefined;
+    this.nextTokens = [];
+    this.indentStack = [0];
+  }
+
+  peek() {
+    if (this.nextTokens.length > 0) {
+      return this.nextTokens[0];
+    }
+
+    if (this.currentLineLexer) {
+      const lineNext = this.currentLineLexer.peek();
+      if (lineNext) {
+        return lineNext;
+      }
+    }
+
+    this.currentLineLexer = undefined;
+
+    while(this.index < this.lines.length) {
+      const line  = this.lines[this.index];
+      const lexer = new LineLexer(line);
+      this.index++;
+      if (this.isBlank(line)) {
+        continue;
+      }
+      
+      if (lexer.peek() === undefined) {
+        continue;
+      }
+
+      this.currentLineLexer = lexer;
+      this.handleIndent(lexer);
+      return this.peek();
+    }
+
+    if (this.currentLineLexer === undefined) {
+      while (this.indentStack.length > 0 && this.indentStack[this.indentStack.length - 1] > 0) {
+        this.nextTokens.push(new Token("dedent", this.indentStack.pop()));
+      }
+    }
+
+    return this.nextTokens[0];
+  }
+
+  next() {
+    const result = this.peek();
+
+    if (this.nextTokens.length > 0) {
+      this.nextTokens.shift();
+      return result;
+    }
+
+    if (this.currentLineLexer) {
+      this.currentLineLexer.next();
+    }
+
+    return result;
+  }
+
+  handleIndent(lineLexer) {
+    if (lineLexer.indent % 2 !== 0) {
+      this.nextTokens.push(new Token("Invalid indentation", lineLexer.indent, true));
+    }
+
+    let currentIndent = this.indentStack[this.indentStack.length - 1];
+
+    if (lineLexer.indent > currentIndent) {
+      this.indentStack.push(lineLexer.indent);
+      this.nextTokens.push(new Token("indent", lineLexer.indent));
+    }
+
+    while (lineLexer.indent < currentIndent) {
+      const lastIndent = this.indentStack.pop();
+      currentIndent = this.indentStack[this.indentStack.length - 1];
+      if (lineLexer.indent > currentIndent) {
+        this.nextTokens.push(new Token("Invalid indentation", lineLexer.indent, true));
+      } else {
+        this.nextTokens.push(new Token("dedent", lastIndent));
+      }
+    }
   }
 
   tokenize() {
     const tokens = [];
-    const indentStack = [0];
 
-    while (this.index < this.lines.length) {
-      const line = this.lines[this.index];
-      let currentIndent = indentStack[indentStack.length - 1];
-
-      if (this.isBlank(line)) {
-        this.index++;
-        continue;
-      }
-      const lineLexer = new LineLexer(line);
-      const lineTokens = lineLexer.tokenize();
-
-      if (lineLexer.indent % 2 !== 0) {
-        tokens.push(new Token("Invalid indentation", lineLexer.indent, true));
-      }
-
-      if (lineLexer.indent > currentIndent) {
-        indentStack.push(lineLexer.indent);
-        tokens.push(new Token("indent", lineLexer.indent));
-      }
-
-      while (lineLexer.indent < currentIndent) {
-        const lastIndent = indentStack.pop();
-        currentIndent = indentStack[indentStack.length - 1];
-        if (lineLexer.indent > currentIndent) {
-          tokens.push(new Token("Invalid indentation", lineLexer.indent, true));
-        } else {
-          tokens.push(new Token("dedent", lastIndent));
-        }
-      }
-
-      tokens.push(...lineTokens);
-      this.index++;
-    }
-
-    while (indentStack.length > 0 && indentStack[indentStack.length - 1] > 0) {
-      tokens.push(new Token("dedent", indentStack.pop()));
+    for (let t = this.next(); t; t = this.next()) {
+      tokens.push(t);
     }
 
     return tokens;
