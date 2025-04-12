c2367e28f64b3906ac9379da2e20b0800971ff94
complexitycollapse <complexitycollapse@protonmail.com>
2025-04-12 23:05:28 +0100

    GMUX: Panel groups and layout

--- editor/gmux/gmux.js
+++ editor/gmux/gmux.js
@@ -1,29 +1,103 @@
-let gmuxContainer = document.getElementById("gmux");
-const windows = [];
-let viewportCols, viewportLines, charWidth, lineHeight;
-const zBasic = 1000, zForward = 2000, zBack = 3000, zTop = 4000;
+import { Group } from "./groups.js";
 
-function addWindow() {
-  const window = {
-    panels: [], 
-    addPanel(props) {
-      const panel = { ...props };
-      panel.z = props.z || zBasic;
-      panel.element = document.createElement("div");
-      panel.element.className = "gpanel";
-      panel.element.style.zIndex = panel.z;
-      gmuxContainer.appendChild(panel.element);
-      if (panel.cols && panel.lines) {
-        sizePanel(panel, panel.cols, panel.lines);
+let gmuxContainer = document.getElementById("gmux");
+const sessions = [];
+let currentWindow;
+let viewportCols, viewportLines, charWidth, lineHeight;
+
+// There are sessions, which contain windows, which contain groups.
+// A group can contain a panel or other groups. Groups lay out their
+// contents either horizontally or vertically. A group can specify
+// its dimensions or max/min dimensions, or nothing at all. The parent
+// group will distribute space within the group among its children
+// subject to these constraints.
+
+// TODO: stacks of groups, each with a different z-order, that allow
+// panels to be overlaid on other panels without disturbing their
+// layout.
+
+// TODO: keyboard input. This will provide commands to create new
+// windows and add content to them.
+
+function addSession() {
+  const session = {
+    windows: [],
+    addWindow(group) {
+      const win = addWindow(session, group);
+      return win;
+    },
+    removeWindow(win) {
+      const index = this.windows.indexOf(win);
+      if (index !== -1) {
+        this.windows.splice(index, 1);
+        gmuxContainer.removeChild(win.element);
       }
-      if (panel.col !== undefined && panel.line !== undefined) {
-        positionPanel(panel, panel.col, panel.line);
-      }
-      window.panels.push(panel);
-      return panel;
     }
   };
+  sessions.push(session);
+  return session;
+}
+
+function addWindow(session, group) {
+  const window = {
+    group,
+    addGroup(parentName, name, members, props) {
+      window.group.get(parentName).add(name, members, props);
+      window.group.doLayout();
+    },
+    addPanel(groupName, panel) {
+      const group = window.group.get(groupName);
+      if (group) {
+        group.setPanel(panel);
+        gmuxContainer.appendChild(panel.element);
+      }
+    },
+    removePanel(groupName) {
+      const group = window.group.get(groupName);
+      if (group) {
+        group.removePanel();
+        gmuxContainer.removeChild(group.panel.element);
+      }
+    }
+  };
+  session.windows.push(window);
   return window;
+}
+
+function Panel() {
+  const panel = {};
+  panel.element = document.createElement("div");
+  panel.element.className = "gpanel gtext";
+  panel.setDimensions = function (group) {
+    panel.col = group.col;
+    panel.line = group.line;
+    panel.cols = group.cols;
+    panel.lines = group.lines;
+    panel.z = group.z;
+    positionPanelElement(panel);
+  };
+  return panel;
+}
+
+function positionPanelElement(panel) {
+  panel.element.style.width = `${panel.cols * charWidth}px`;
+  panel.element.style.height = `${panel.lines * lineHeight}px`;
+  panel.element.style.left = `${panel.col * charWidth}px`;
+  panel.element.style.top = `${panel.line * lineHeight}px`;
+  panel.element.zOrder = getZOrder(panel.z);
+}
+
+function getZOrder(z) {
+  switch (z) {
+    case "basic":
+      return 1000;
+    case "forward":
+      return 2000;
+    case "back":
+      return 0;
+    case "top":
+      return 4000;
+  }
 }
 
 function measureCharSize() {
@@ -42,50 +116,32 @@ function updateViewportSize() {
   viewportLines = Math.floor(window.innerHeight / lineHeight);
 }
 
-window.addEventListener("resize", updateViewportSize);
+window.addEventListener("resize", () => {
+  updateViewportSize();
+  currentWindow.group.cols = viewportCols;
+  currentWindow.group.lines = viewportLines;
+  currentWindow.group.doLayout();
+});
 window.addEventListener("DOMContentLoaded", () => {
   measureCharSize();
   updateViewportSize();
   const width = 80;
-  const win = addWindow();
-  buildWindowFromLayout([
-    {
-      col: 1,
-      line: 1,
-      cols:   viewportCols / 2 - 1,
-      lines: (viewportLines - 2) / 2
-    }, {
-      col: (viewportCols / 2) + 1,
-      line: 1,
-      cols: viewportCols / 2 - 1,
-      lines: viewportLines - 2
-    }, {
-      col: 1,
-      line: (viewportLines - 2) / 2 + 1,
-      cols:   viewportCols / 2 - 1,
-      lines: (viewportLines - 2) / 2
-    }
-  ]);
+  const windowSizing = {
+    col: 0,
+    line: 0,
+    cols: viewportCols,
+    lines: viewportLines
+  };
+  currentWindow = addSession().addWindow(
+    Group("window", "vertical", [], windowSizing, windowSizing));
+
+  currentWindow.addGroup("window", "top-margin", [], { lines: 1 });
+  currentWindow.addGroup("window", "main-area", [], {});
+  currentWindow.addGroup("main-area", "left-margin", [], { });
+  currentWindow.addGroup("main-area", "focus", [], { cols: width });
+  currentWindow.addGroup("main-area", "focus2", [], { cols: width });
+  currentWindow.addGroup("main-area", "right-margin", [], { });
+  currentWindow.addGroup("window", "bottom-margin", [], { lines: 1 });
+  currentWindow.addPanel("focus", Panel());
+  currentWindow.addPanel("focus2", Panel());
 });
-
-function sizePanel(panel, cols, lines) {
-  panel.cols = cols;
-  panel.lines = lines;
-  panel.element.style.width = `${cols * charWidth}px`;
-  panel.element.style.height = `${lines * lineHeight}px`;
-}
-
-function positionPanel(panel, col, line) {
-  panel.col = col;
-  panel.line = line;
-  panel.element.style.left = `${col * charWidth}px`;
-  panel.element.style.top = `${line * lineHeight}px`;
-}
-
-function buildWindowFromLayout(layout) {
-  const win = addWindow();
-  for (const panel of layout) {
-    const newPanel = win.addPanel(panel);
-  }
-  return win;
-}
---
+++ editor/gmux/groups.js
@@ -0,0 +1,147 @@
+export function Group(name, direction, members, sizing, props = {}) {
+  const group = {
+    ...props,
+    sizing,
+    type: "group",
+    name,
+    direction,
+    members,
+    get(name) {
+      if (name === group.name) {
+        return group;
+      } else {
+        for (const member of group.members) {
+          const found = member.get(name);
+          if (found) {
+            return found;
+          }
+        }
+      }
+    },
+    add(name, members, props) {
+      const newGroup = Group(name, flip(group.direction), members, props);
+      group.members.push(newGroup);
+      return newGroup;
+    },
+    setPanel(panel) {
+      group.panel = panel;
+      group.doLayout();
+    },
+    removePanel() {
+      group.panel = undefined;
+      group.doLayout();
+    },
+    doLayout() {
+      const panel = group.panel;
+      if (panel) {
+        panel.line = group.line;
+        panel.col = group.col;
+        panel.lines = group.lines;
+        panel.cols = group.cols;
+        panel.z = group.z;
+        panel.setDimensions(group);
+      } else {
+        recalculateLayout(group);
+      }
+      group.members.forEach(m => m.doLayout());
+    },
+    getDimensions(direction, sizing) {
+      const obj = sizing ? group.sizing : group;
+      if (direction === "horizontal") {
+        return {
+          pos: obj.col,
+          size: obj.cols,
+          min: obj.minCols,
+          max: obj.maxCols,
+          original: group
+        };
+      } else {
+        return {
+          pos: obj.line,
+          size: obj.lines,
+          min: obj.minLines,
+          max: obj.maxLines,
+          original: group
+        };
+      }
+    },
+    setDimensions(direction, pos, size) {
+      if (direction === "horizontal") {
+        group.col = pos;
+        group.cols = size;
+      } else {
+        group.line = pos;
+        group.lines = size;
+      }
+    }
+  }
+
+  return group;
+}
+
+function recalculateLayout(group) {
+  const groupDimensions = group.getDimensions(group.direction);
+  const otherDimensions = group.getDimensions(flip(group.direction));
+  const memberDimensions = group.members.map(m => m.getDimensions(group.direction, true));
+  calculateSizes(memberDimensions, groupDimensions.size);
+
+  let pos = groupDimensions.pos;
+  memberDimensions.forEach(d => {
+    // Dimensions along the group axis are set according to the calculation.
+    d.original.setDimensions(group.direction, pos, d.size);
+    // Dimensions along the other axis are equal to the group's.
+    d.original.setDimensions(
+      flip(group.direction),
+      otherDimensions.pos,
+      otherDimensions.size);
+    pos += d.size;
+  });
+}
+
+function calculateSizes(dimensions, groupSize) {
+  // Find out the total minimum size of all the members and the remaining free space.
+  const usedSpace = sum(dimensions.map(m => m.size ?? m.min));
+  const freeSpace = groupSize - usedSpace;
+
+  // If there is no free space left, everything gets the minimum size.
+  if (freeSpace <= 0) {
+    dimensions.forEach(m => m.size = m.size ?? m.min ?? 1);
+    return;
+  }
+
+  // Members that don't hae a size set will need to have one calculated.
+  const requireResize = dimensions.filter(m => !m.size);
+
+  // Attempt to distribute the remaining space evenly, respecting the maximum
+  // size of each member.
+  while (freeSpace > 0 && requireResize.length > 0) {
+    const padding = freeSpace / requireResize.length;
+    const undersized = requireResize.filter(m => m.max && m.max < padding);
+    if (undersized.length === 0) {
+      requireResize.forEach(m => {
+        m.size = padding;
+      });
+      break;
+    }
+    undersized.forEach(m => {
+      m.size = m.max;
+      freeSpace -= m.size;
+      removeItem(requireResize, m);
+    });
+  }
+}
+
+function flip(direction) {
+  return direction === "horizontal" ? "vertical" : "horizontal";
+}
+
+function removeItem(arr, item) {
+  const index = arr.indexOf(item);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+}
+
+function sum(arr) {
+  return arr.reduce((acc, val) => acc + (val ?? 0), 0);
+}
