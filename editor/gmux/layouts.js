070052426f09ce35d49c741dae4c1e4bbf2d12dc
complexitycollapse <complexitycollapse@protonmail.com>
2025-05-21 22:45:59 +0100

    GMUX: main window layer

--- editor/gmux/layouts.js
+++ editor/gmux/layouts.js
@@ -1,6 +1,53 @@
 import { addSession, showWindow, Panel, onLoaded } from "./sessions.js";
 import { Stack, Group } from "./groups.js";
 
+function addMainWindowLayer(window, stack) {
+  const defaultWidth = 120;
+
+  stack.add("main-window-layer", [
+    Group("main-area", "horizontal", [
+      Group("left-margin", "vertical", []),
+      Group("main-window", "vertical", [], { cols: defaultWidth }),
+      Group("aux-window", "vertical", [], { cols: defaultWidth }),
+      Group("right-margin", "vertical", [])
+    ])
+  ], {}, { z: "basic" });
+
+  const aux = window.getGroup("aux-window");
+  aux.visible = false;
+  
+  const mainPanel = Panel(), auxPanel = Panel();
+  window.addPanel("main-window", mainPanel);
+  window.addPanel("aux-window", auxPanel);
+  
+  const obj = {
+    mainPanel,
+    auxPanel,
+    width: defaultWidth,
+    requestedWidth: defaultWidth,
+    setWidth(width) {
+      obj.requestedWidth = width;
+      // if (aux.visible && window.group.cols < width * 2) {
+      //   width = Math.floor(window.group.cols / 2);
+      // }
+      obj.width = width;
+      window.getGroup("main-window").setSize({ cols: width });
+      window.getGroup("aux-window").setSize({ cols: width });
+    },
+    showAux() {
+      aux.visible = true;
+      window.getGroup("main-area").doLayout();
+    },
+    hideAux() {
+      aux.visible = false;
+      window.getGroup("main-area").doLayout();
+    }
+  };
+
+  obj.setWidth(defaultWidth);
+  return obj;
+}
+
 function addAlertLayer(window, stack) {
   stack.add("alert-layer", [
     Group("alerts", "vertical", [
@@ -34,14 +81,13 @@ function addAlertLayer(window, stack) {
 
 onLoaded(() => {
   const window = addSession().addWindow();
-  const stack = Stack("layers", [
-    Group("main-area", "horizontal", [], {}, { z: "basic" })
-  ]);
+  const stack = Stack("layers", []);
 
   window.addGroup("window", "top-margin", [], { lines: 1 });
   window.addGroup("window", "centre-bar", [stack]);
   window.addGroup("window", "bottom-margin", [], { lines: 1 });
 
+  const main = addMainWindowLayer(window, stack);
   const alert = addAlertLayer(window, stack);
   alert.show(undefined, undefined, 30, 10);
   showWindow(window);
